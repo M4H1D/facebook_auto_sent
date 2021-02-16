9e49c81e0cd91de9799c3df2db9fee24f2214e7f
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-02-17 04:40:40 +0600

    Add files via upload

---
+++ messenger_auto_message.py
@@ -0,0 +1,10 @@
+#1st of all you goto terminal and type"pip install pyautogui"
+import pyautogui as py
+import time
+while True:
+	time.sleep(5) #this 5 means second 
+	py.typewrite("Assalamu Alikum")
+	py.press("enter")
+#write the code then press run and go to messenger where i sent message and Press mouse cursor in write box. put the cursor in "write message" box
+#this will be continuously held after 5 second 
+#if we close this programme then you press the red pause button in pycharm under and beside the output box.
